70ce9d1ca1533ff58d23de0e88077f18890692f1
VespaCaution <BENHUPKA@HOTMAIL.COM>
2026-07-27 13:48:36 +0200

    Add service worker registration in Script.js
    Register service worker on window load and handle success/error.

--- Script.js
+++ Script.js
@@ -1048,4 +1048,28 @@ console.log(
 
 /*==========================================================
     FIN DU SCRIPT
+==========================================================*//*==========================================================
+    ENREGISTREMENT DU SERVICE WORKER
 ==========================================================*/
+
+if("serviceWorker" in navigator){
+
+    window.addEventListener("load",()=>{
+
+        navigator.serviceWorker.register("./service-worker.js")
+
+            .then(()=>{
+
+                console.log("Service Worker enregistré.");
+
+            })
+
+            .catch(error=>{
+
+                console.error(error);
+
+            });
+
+    });
+
+}
